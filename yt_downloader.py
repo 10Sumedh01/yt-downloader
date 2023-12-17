201282ac9c0b0e47785900709f6e286fccf65160
Sumedh01 <sumedhwature@gmail.com>
2023-12-17 12:54:03 +0530

    Dec 17 update yt downlader UI (CLI)

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,15 +1,45 @@
 from pytube import YouTube
 from pytube import Playlist
+from pytube.exceptions import VideoUnavailable
+from colorama import Fore
+import pyfiglet
+import inquirer
+from inquirer.themes import GreenPassion
 
-a = input('\nEnter "S" for single video or "P" for playlist : ').lower()
-print(a)
 
-if a =='s':
+
+
+#Display Header
+f = pyfiglet.Figlet(font="ogre", width=80)
+x = (f.renderText('    YT Downloader'))
+print(Fore.YELLOW+'      ----------------------------------------------------------------')
+print(Fore.RED+ x)
+print(Fore.YELLOW+'      ----------------------------------------------------------------') 
+
+#Logic Part
+question = [
+    inquirer.List("Video-Type",message="What Type Of Video You Wand To Download", choices=["Single Video", "Playlist"]),
+]
+
+usersChoice = inquirer.prompt(question, theme=GreenPassion())
+print(usersChoice['Video-Type'])
+
+a = usersChoice['Video-Type'].lower()
+
+if a =='single video':
     yt = YouTube(input('\nEnter the URL of the video : '))
-    yt.streams.get_highest_resolution().download()
-elif a =='p':
+    try:
+        yt.streams.get_highest_resolution().download()
+    except VideoUnavailable:
+        print(Fore.RED+"Video not available")
+elif a =='playlist':
     pl = Playlist(input('\nEnter the URL of the playlist : '))
     for video in pl.videos:
-        video.streams.get_highest_resolution().download()
+        try:
+            video.streams.get_highest_resolution().download()
+        except VideoUnavailable:
+            print(Fore.RED+"Video not available")
 else:
     print('\nInvalid input')
+
+
