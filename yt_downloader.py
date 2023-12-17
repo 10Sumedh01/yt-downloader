645fe0db5205ccd551153776f38f67010de24c24
Sumedh01 <sumedhwature@gmail.com>
2023-12-17 18:37:18 +0530

    Added progress bar

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,6 +1,7 @@
 from pytube import YouTube
 from pytube import Playlist
 from pytube.exceptions import VideoUnavailable
+from pytube.cli import on_progress
 from colorama import Fore
 import pyfiglet
 import inquirer
@@ -17,6 +18,13 @@ print(Fore.RED+ x)
 print(Fore.YELLOW+'      ----------------------------------------------------------------') 
 
 #Logic Part
+# def progress_function(video_stream, total_size, bytes_remaining):
+#     total_size = video_stream.filesize
+#     bytes_downloaded = total_size - bytes_remaining
+#     percent = (bytes_downloaded / total_size)*100
+#     print('\r'+"▌" * int(percent)+ " " * (100-int(percent))+ "{}%".format(int(percent)),end='')
+
+
 question = [
     inquirer.List("Video-Type",message="What Type Of Video You Wand To Download", choices=["Single Video", "Playlist"]),
 ]
@@ -27,19 +35,19 @@ print(usersChoice['Video-Type'])
 a = usersChoice['Video-Type'].lower()
 
 if a =='single video':
-    yt = YouTube(input('\nEnter the URL of the video : '))
+    yt = YouTube(input(Fore.YELLOW+'\nEnter the URL of the video : '),on_progress_callback=on_progress)
     try:
         yt.streams.get_highest_resolution().download()
     except VideoUnavailable:
         print(Fore.RED+"Video not available")
 elif a =='playlist':
-    pl = Playlist(input('\nEnter the URL of the playlist : '))
+    pl = Playlist(input(Fore.YELLOW+'\nEnter the URL of the playlist : '),on_progress_callback=on_progress)
     for video in pl.videos:
         try:
             video.streams.get_highest_resolution().download()
         except VideoUnavailable:
             print(Fore.RED+"Video not available")
 else:
-    print('\nInvalid input')
+    print(Fore.RED+'\nInvalid input')
 
 
